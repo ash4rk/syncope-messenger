31204e277afbd60c336052a352009c919ffd271f
ash4rk <cool.shark.98@mail.ru>
2023-05-06 01:36:17 +0300

    FEAT: Add spdlog lib via conan

--- CommonNetworking/include/CommonNetworking/Base.h
+++ CommonNetworking/include/CommonNetworking/Base.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <spdlog/spdlog.h>
+
 enum class MyEnum {
 	One,
 	Two,
 	Three	
 };
+
+void print_spdlog() { spdlog::info("Welcome to spdlog!"); }
+
--- SyncopeClient/main.cpp
+++ SyncopeClient/main.cpp
@@ -4,5 +4,6 @@
 int main() {
   std::cout << "Hello, World! I am client!" << std::endl;
   std::cout << static_cast<int>(MyEnum::One) << std::endl;
+  print_spdlog();
   return 0;
 }
